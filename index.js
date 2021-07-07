bb31f10d391ca7fc9e370af403d1c3581324348c
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-07-07 10:09:50 -0700

    index in progress

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import puppeteer from 'puppeteer';
-import { JSDOM } from 'jsdom';
-import fetch from 'node-fetch';
-import ingest from './app.js';
-import  fs  from 'fs';
+
 
 const myEmail = process.env.username;
 const myPass = process.env.password;
@@ -18,16 +15,18 @@ const marketplace = 'https://www.facebook.com/marketplace';
   const browser = await puppeteer.launch({ headless: false, defaultViewport: null });
   const page = await browser.newPage();
   await page.goto(marketplace);
-  //   await page.evaluate(() => {
-  //     const elements = document.getElementsByName('div');
-  //   });
-  const details = await ingest(marketplace);
-  const dom = new JSDOM(details);
-  const document = dom.window.document;
-  const email = document.getElementsByName('email');
-  const password = document.getElementsByName('pass');
+
+  await page.waitForSelector('input[name=email]');
+
+  page.evaluate(() => {
+    // const email = document.querySelector('input[name=email]');
+    const email = document.getElementsByName('email');
+    // const password = document.getElementsByName('pass');
+    return email.innerHTML;
+    
+  })
+    .then(email => console.log(email));
   
-  fs.writeFile(details);
 
 //   await page.type(email, myEmail);
 //   await page.type(password, myPass);
