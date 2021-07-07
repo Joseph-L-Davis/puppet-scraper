d0ddc7f6e73aef4a3c233365edbd4e2527b410f0
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-07-07 10:09:01 -0700

    app.js grabs img alt tags

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-import fetch from 'node-fetch';
+import puppeteer from 'puppeteer';
+(async () => {
+  //opens actual chromium browser
+  const browser = await puppeteer.launch({ headless: false });
+  //opens a new page
+  const page = await browser.newPage();
 
-export default async function ingest(url) {
-  const res = await fetch(url);
-  return res.text();
-}
-
+  await page.goto('http://www.facebook.com/marketplace', { waitUntil: 'networkidle0' });
+  //try to get unique identifiers
+  const grabMarketpalceEntries = await page.evaluate(() => {
+    const titleTags = [...document.querySelectorAll('img')].map(element => element.alt);
+    //  const titles = []
+    //  titleTags.forEach((element) => {
+    //    titles.push(element.innerHTML)
+    //  })
+    return titleTags;
+  });
+  console.log(grabMarketpalceEntries); 
+  await browser.close();
+})();
